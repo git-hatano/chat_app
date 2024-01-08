1beb05e304685bb4058d71a9ba92fc32b8817414
kazuhiro <hatano.kazuhiro817@gmail.com>
2024-01-08 22:21:45 +0900

    Add ask pdf

---
+++ test_emmbedding.py
@@ -0,0 +1,7 @@
+from langchain.embeddings import OpenAIEmbeddings
+
+emb_model = OpenAIEmbeddings()
+text = "こんにちは世界！"
+result = emb_model.embed_documents([text])  # listで入れる必要があるので注意
+print(f'Embeddingの次元数: {len(result[0])}')
+print(result[0][:5])
